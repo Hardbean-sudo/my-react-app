282710e3fd3f86923985a6ad2f1cb2371eaf5f08
hardbean <betancourtharbin@gmail.com>
2025-11-23 18:25:21 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+
+const town = (
+  <h3>Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu</h3>
+);
+
+createRoot(document.getElementById('root')).render(
+    town
+)
