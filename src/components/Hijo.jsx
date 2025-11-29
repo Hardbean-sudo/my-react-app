74da490024fc178d30ddcd6783abdbb1d31265ff
Hardbean-sudo <betancourtharbin@gmail.com>
2025-11-28 19:22:18 -0500

    Add asset list to Hijo component for improved rendering

--- src/components/Hijo.jsx
+++ src/components/Hijo.jsx
@@ -2,10 +2,14 @@ const Hijo = ({ children }) => {
     const wow = (a, b) => {
         alert(a + " " + b.type);
     }
+    const assets = ["Casa", "Carro", "Negocio", "Vending Machine", "IA", "Online Store"];
     return (
         <>
         <div>{children}</div>
         <button onClick={(event) => wow("Yeah!", event)}>Alertar!</button>
+        <ul>
+            {assets.map((assetsItem, index) => <li key={index}>{assetsItem}</li>)}
+        </ul>
         </>
     );
 }
