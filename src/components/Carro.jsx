1a8b9282ed1ab7a8f66f309f7b24f92e75ebf3a9
Hardbean-sudo <betancourtharbin@gmail.com>
2025-12-01 20:46:47 -0500

    Add newCar state and update functionality in Carro component

--- src/components/Carro.jsx
+++ src/components/Carro.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
 const Carro = ({color = "blue", carinfo}) => {
+  const [newCar, setNewCar] = useState({
+    name: "Lamborghini",
+    model: "Aventador",
+    year: "2020",
+    color: "red",
+  });
+  const [fula, setFula] = useState("yellow");
+  const handleChangeFula = (e) => {
+    setFula(e.target.value);
+  }
+  const updateNewCar = (c) => {
+    setNewCar(prevSate => ({
+      ...prevSate,
+      color: `${c || "yellow" }`,
+    }));
+  }
   return (
     <>
       <h2>My {carinfo.name} {carinfo.model}!</h2>
       <p>It is {color} and it is from {carinfo.year}!</p>
+      <div>
+      <h2>My second car is a {newCar.name}</h2>
+      <p>It is a {newCar.color} {newCar.model} from {newCar.year}!</p>
+      <input type="text" value={fula} onChange={handleChangeFula} />
+      <button onClick={() => updateNewCar(fula)}>Update New Car</button>
+      </div>
     </>
   );
 }
