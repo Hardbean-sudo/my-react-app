8a1f2ee6497c409d7743c72adb8022f0d3eb6e0b
Hardbean-sudo <betancourtharbin@gmail.com>
2026-01-11 22:10:15 -0500

    Refactor code structure for improved readability and maintainability

--- src/components/Carro.jsx
+++ src/components/Carro.jsx
@@ -19,17 +19,31 @@ function Counter() {
 }
 function Timer() {
   const [counter, setCounter] = useState(0);
+  const [colorPositive, setColorPositive] = useState(true);
 
   useEffect(() => {
     let timer = setTimeout(() => {
       setCounter((counter) => counter + 1);
-    }, 1000);
+    }, 25);
     return () => clearTimeout(timer);
   }, [counter]); // <- add empty brackets here
-
-  return <h1>I've rendered {counter} times!</h1>;
+  if (counter > 359) {
+    setCounter(0);
+    setColorPositive(!colorPositive);
+  }
+  const colorGrade = colorPositive ? counter * 1 : counter * -1;
+  return (
+    <>
+      <h1>I've rendered {counter} times!</h1>
+      <br />
+      <img src="\public\pineapple.jpg" alt="A golden pineapple fruit rotating through a spectrum of hues, displayed as a five-pointed star shape against a plain background" style={{ filter: `hue-rotate(${colorGrade}deg)`, clipPath: `polygon(72.5% 0%, 100% 40%, 90% 90%, 55% 90%, 45% 40%)` }} />
+      <img src="\public\pineapple.jpg" alt="A pineapple fruit with inverted colors becoming progressively more saturated, illustrating a visual effect demonstration" style={{ filter: `invert(${counter}%)`, clipPath: `polygon(72.5% 0%, 100% 40%, 90% 90%, 55% 90%, 45% 40%)` }} />
+      <br />
+      <p>{colorGrade}</p>
+    </>
+  );
 }
-const Carro = ({color = "blue", carinfo}) => {
+const Carro = ({ color = "blue", carinfo }) => {
   const [newCar, setNewCar] = useState({
     name: "Lamborghini",
     model: "Aventador",
@@ -43,7 +57,7 @@ const Carro = ({color = "blue", carinfo}) => {
   const updateNewCar = (c) => {
     setNewCar(prevSate => ({
       ...prevSate,
-      color: `${c || "yellow" }`,
+      color: `${c || "yellow"}`,
     }));
   }
   return (
@@ -53,10 +67,10 @@ const Carro = ({color = "blue", carinfo}) => {
       <Timer />
       <Counter />
       <div>
-      <h2>My second car is a {newCar.name}</h2>
-      <p>It is a {newCar.color} {newCar.model} from {newCar.year}!</p>
-      <input type="text" value={fula} onChange={handleChangeFula} />
-      <button onClick={() => updateNewCar(fula)}>Update New Car</button>
+        <h2>My second car is a {newCar.name}</h2>
+        <p>It is a {newCar.color} {newCar.model} from {newCar.year}!</p>
+        <input type="text" value={fula} onChange={handleChangeFula} />
+        <button onClick={() => updateNewCar(fula)}>Update New Car</button>
       </div>
     </>
   );
